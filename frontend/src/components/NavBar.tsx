74565ae211f77d2dd90ac23e8b03440870a3c729
Csoldo <nagyboldi2000@gmail.com>
2024-02-16 19:01:31 +0100

    upgrade navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,18 +1,125 @@
-import { HStack } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import ColorModeSwitch from "./ColorModeSwitch";
-import LanguageSelector from "./LanguageSelector";
+"use client";
 
-const NavBar = () => {
-  return (
-    <HStack padding="10px">
-      <Link to="/">Home</Link>
-      <Link to="/portfolio">Portfolio</Link>
-      <Link to="/about">About</Link>
-      <ColorModeSwitch />
-      <LanguageSelector />
-    </HStack>
-  );
+import {
+  Box,
+  Flex,
+  Avatar,
+  Button,
+  Menu,
+  MenuButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  HStack,
+  IconButton,
+} from "@chakra-ui/react";
+import { CloseIcon, HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Links = ["Home", "Portfolio", "About"];
+
+const mapLinkToPath = (link: string) => {
+  switch (link) {
+    case "Home":
+      return "/";
+    case "Portfolio":
+      return "/portfolio";
+    case "About":
+      return "/about";
+    default:
+      return "/";
+  }
 };
 
-export default NavBar;
+const NavLink = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={mapLinkToPath(children as string) || "#"}
+    >
+      {children}
+    </Box>
+  );
+};
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: "none" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>Logo</Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Avatar size={"sm"} src={"../assets/boldizsar-nagy.jpg"} />
+                </MenuButton>
+                {/* <MenuList alignItems={"center"}>
+                  <br />
+                  <Center>
+                    <Avatar
+                      size={"2xl"}
+                      src={"frontend/src/assets/boldizsar-nagy.jpg"}
+                    />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>Username</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Your Servers</MenuItem>
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList> */}
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
