a72bbe9c96c819f2f452dabfc9b565d27f1c3078
Csoldo <nagyboldi2000@gmail.com>
2024-02-18 09:05:06 +0100

    dropdown language selector with flags

--- frontend/src/components/LanguageSelector.tsx
+++ frontend/src/components/LanguageSelector.tsx
@@ -1,7 +1,15 @@
 import { useTranslation } from "react-i18next";
 import enFlag from "../assets/en_flag.png";
 import huFlag from "../assets/hu_flag.png";
-import { Box, Select } from "@chakra-ui/react";
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Image,
+  Button,
+} from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
@@ -13,22 +21,28 @@ const LanguageSelector = () => {
   const languageOptions = [
     { value: "en", label: "English", flag: enFlag },
     { value: "hu", label: "Magyar", flag: huFlag },
-    // Add more languages as needed
   ];
 
   return (
-    <Box>
-      <Select
-        defaultValue={i18n.language}
-        onChange={(e) => changeLanguage(e.target.value)}
-      >
+    <Menu isLazy>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+        {i18n.language === "en" ? "English" : "Magyar"}
+      </MenuButton>
+      <MenuList p={0} minW="0" w={"140px"}>
         {languageOptions.map((lang) => (
-          <option key={lang.value} value={lang.value}>
-            {lang.label}
-          </option>
+          <MenuItem key={lang.value} onClick={() => changeLanguage(lang.value)}>
+            <span>{lang.label}</span>
+            <Image
+              boxSize="2rem"
+              borderRadius="2px"
+              src={lang.flag}
+              alt={lang.label}
+              ml="16px"
+            />
+          </MenuItem>
         ))}
-      </Select>
-    </Box>
+      </MenuList>
+    </Menu>
   );
 };
 
